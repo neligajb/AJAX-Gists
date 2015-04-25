f788235022bf8f8ff2f882a91c1bf8694a103d54
Boone N <boone.neligan@gmail.com>
2015-04-25 19:33:03 -0400

    styled gist list and completed JSON parsing for the gist objects

--- gistsJS.js
+++ gistsJS.js
@@ -1,11 +1,47 @@
 var originalGistList = [];
 
+function gistHTML(parent_div, gist) {
+	var gistDiv = document.createElement('div');
+	gistDiv.setAttribute('id', 'gist-div');
+
+	var fbutton = document.createElement('button');
+	fbutton.innerHTML = '+';
+	fbutton.setAttribute("gistID", gist.id);
+	fbutton.onclick = function() {
+		var gistID = this.getAttribute("gistID");
+		var toBeFavoredGist = findByID(gistID);
+		addFavoriteGist(toBeFavoredGist);
+	}
+
+	var desc = document.createElement('p');
+	var link = document.createElement('a');
+
+	link.setAttribute('href', gist.url);
+	link.setAttribute('title', 'gist link');
+	link.innerHTML = gist.url;
+
+	var br = document.createElement('br');
+
+	desc.innerText = gist.description;
+
+	gistDiv.appendChild(fbutton);
+	gistDiv.appendChild(desc);
+	gistDiv.appendChild(br);
+	gistDiv.appendChild(link);
+
+	parent_div.appendChild(gistDiv);
+}
+
+function addFavoriteGist(favGist) {
+
+}
+
 function fetchGists() {
 	var req = new XMLHttpRequest;
 	if (!req) {
 		throw 'Unable to get request.';
 	}
-	var pages = document.getElementsByName('num-of-pages')[0];
+	var pages = document.getElementsByName('num-of-pages')[0].value;
 	pages = Math.floor(pages);
 	if (pages < 0 || (typeof pages != 'number')) {
 		pages = 1;
@@ -17,8 +53,13 @@ function fetchGists() {
 	req.onreadystatechange = function() {
 		if(this.readyState === 4) {
 			originalGistList = JSON.parse(this.responseText);
+			var len = originalGistList.length;
+			for (var i = 0; i < len; i++) {
+				gistHTML(document.getElementById('gists'), originalGistList[i]);
+			}
 		}
 	};
 	req.open('GET', url);
 	req.send();
 }
+
